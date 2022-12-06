e4f10b663a9342827f2614c8ff9a78f840c358f7
Dylan D <dore.dylan31@gmail.com>
2022-12-06 14:59:46 +0100

    add title and discount in migration

--- db/migrate/20221206112347_create_promos.rb
+++ db/migrate/20221206112347_create_promos.rb
@@ -1,6 +1,8 @@
 class CreatePromos < ActiveRecord::Migration[7.0]
   def change
     create_table :promos do |t|
+      t.string :name
+      t.float :discount
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_112347) do
   end
 
   create_table "promos", force: :cascade do |t|
+    t.string "name"
+    t.float "discount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
