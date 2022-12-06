5e704f89cab917551ca04b313566b4c76b5c1314
JBE29 <jeanbaptiste.emily@gmail.com>
2022-12-06 17:14:04 +0100

    modification on order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
     belongs_to :user
+    has_many :driving_courses, through :order_items
+    has_many :options, through :order_items
     has_many :order_items
-    belongs_to :payment_detail
+    #belongs_to :payment_detail
 
 end
