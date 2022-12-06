c095c7a379bbc932d9ec998b3b968a65b185cad7
Dylan D <dore.dylan31@gmail.com>
2022-12-06 16:58:58 +0100

    write order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+    belongs_to :user
+    has_many :order_items
+    has_many :driving_courses, through :order_items
+    has_many :options, through :order_items
 end
