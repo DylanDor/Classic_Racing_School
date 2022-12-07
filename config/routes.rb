d4fb0e1c9467fbce85b7e155e85b6f86c638bc7c
YannGUIOT <guiot_yann@hotmail.fr>
2022-12-07 16:09:26 +0100

    Stripe creation

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   resources :driving_courses
   resources :carts
 
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+  end
+
   root "driving_courses#index"
 
 end
