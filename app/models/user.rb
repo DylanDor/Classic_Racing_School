89b644b9e999a1b3413127dcd2790bfbec124131
Dylan D <dore.dylan31@gmail.com>
2022-12-09 15:40:38 +0100

    remove initialize cart from controller to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :cart
+
+  private
+
+  after_create do |user|
+    self.cart ||= Cart.create(user: user)
+  end
 end
