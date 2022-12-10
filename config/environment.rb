62998d341eca023691c71c612d5719748f5344c5
MaevaGilles <maevagilles-web@hotmail.fr>
2022-12-10 02:44:45 +0100

    Action Mailer Settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDINBLUE_LOGIN'],
+    :password => ENV['SENDINBLUE_PWD'],
+    :domain => 'https://classic-racing-school.fly.dev/',
+    :address => 'smtp-relay.sendinblue.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
