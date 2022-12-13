6b82da2d41098999158a88779da8c565c22107a1
Dylan D <dore.dylan31@gmail.com>
2022-12-13 13:20:01 +0100

    fix deploy and revert admin

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,8 @@
 class Cart < ApplicationRecord
     belongs_to :user
+    has_many :cart_items
     has_many :driving_courses, through: :cart_items
     has_many :options, through: :cart_items
-    has_many :cart_items
 
     def total_price
         total = 0
