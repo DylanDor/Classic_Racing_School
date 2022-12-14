233340d57a557cbadcd9f88c50ed3b56db337877
Dylan D <dore.dylan31@gmail.com>
2022-12-14 12:35:31 +0100

    add amin in user table and security access dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_113005) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_094002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,6 +104,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_113005) do
     t.boolean "photo_publication_consent"
     t.string "instagram_nickname"
     t.boolean "personal_data_consent"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
